d08a79e806a25eaf36c156baed783194f167c1d3
Emmanoel Buonafina Tome <emmanoel1965@gmail.com>
2021-07-16 13:01:16 -0300

    protegendo-com-csrf

---
+++ csrf/send.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+$csrf_token = $_SESSION['csrf_token'] ?? false;
+
+if(!$csrf_token or $csrf_token !== filter_input(INPUT_POST, '_csrf_token')) {
+    die('CSRF token validation fail');
+}
+
+include 'libs/get-data.php';
