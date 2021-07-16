e0f71e5eebf6cf111e3291338f93d69da573498f
Emmanoel Buonafina Tome <emmanoel1965@gmail.com>
2021-07-16 15:38:03 -0300

    enviado-email

---
+++ enviando-email/libs/url.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = filter_input(INPUT_POST, 'url');
+
+if (preg_match('/w{0, 3}\.[a-z]+\.[a-z]+[a-z\.]*$/', $url, $matches)) {
+    $url =  $matches[0];
+} else {
+    die('URL IS INVALID');
+}
+
+var_dump($url);
---
+++ enviando-email/send.php
@@ -0,0 +1,8 @@
+<?php
+
+session_start();
+
+include 'libs/csrf.php';
+include 'libs/get-data.php';
+include 'libs/url.php';
+include 'libs/mail.php';
