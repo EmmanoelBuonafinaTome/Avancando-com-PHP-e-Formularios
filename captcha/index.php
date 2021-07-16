c2523524caaa16bd8338cc74885b027752ee5a2a
Emmanoel Buonafina Tome <emmanoel1965@gmail.com>
2021-07-16 14:38:22 -0300

    protecao-com-captcha

---
+++ captcha/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+$_SESSION['csrf_token'] = sha1(rand(1, 200000));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="send.php" method="POST">
+        <input type="hidden" name="_csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+        <input name="nome" type="text" placeholder="Nome"><br>
+        <input name="email" type="email" placeholder="E-mail"><br>
+        <textarea name="descricao" placeholder="Descrição" id="" cols="30" rows="10"></textarea><br>
+        <img src="captcha.php">
+        <input name="_captcha" type="text" placeholder="Digite as letras acima">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
---
+++ captcha/send.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+
+include 'libs/csrf.php';
+include 'libs/captcha.php';
+include 'libs/get-data.php';
